8eb6956d812eaa44c6212c268b54ec1b910fd52e
Richard <shaodeyu@foxmail.com>
2021-02-27 12:57:38 +0800

    画板

---
+++ script.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById("canvas");
+
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+
+let ctx = canvas.getContext("2d");
+
+let painting = false;
+ctx.fillStyle = "blue";
+
+canvas.onmousedown = () => {
+  painting = true;
+};
+
+canvas.onmousemove = (e) => {
+  if (painting === true) {
+    ctx.beginPath();
+    ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI);
+
+    ctx.stroke();
+    ctx.fill();
+  } else {
+    console.log("什么都不做");
+  }
+};
+
+canvas.onmouseup = () => {
+  painting = false;
+};
